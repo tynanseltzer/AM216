e4a1d58f9e9e632e9f778011f793e15803ce3846
tynanseltzer <tynanseltzer@gmail.com>
2018-04-23 10:45:17 -0400

    trying to get hex binning

--- imConvert.py
+++ imConvert.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import os
-x = os.listdir('pics/train')
+x = os.listdir('pics/validation')
 for folder in x:
-    y = os.listdir('pics/train/' + folder)
+    y = os.listdir('pics/validation/' + folder)
     for file in y:
         print (folder)
         print (file)
-        path = 'pics/train/' + folder + '/'
+        path = 'pics/validation/' + folder + '/'
         n = file.split('.')[0]
         im = Image.open(path + file)
+        
         im = im.convert("RGB")
         im.save(path + n + '.jpg')
+        os.remove(path + file)
